37a652137e4c40cf1766bd8df4f22b3889417d1e
Ayush Pandey <ayush12122003@gmail.com>
2024-12-10 04:27:34 +0530

    init

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const express = require('express');
-const path = require('path');
-const crypto = require('crypto-js');
+const express = require("express");
+const crypto = require("crypto-js");
+const path = require("path");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-// Serve the first clue (Base64 decode clue)
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Route to serve the home page (index.html)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Serve the second clue (AES encrypted clue)
-app.get('/page2', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'page2.html'));
+// Route to serve the second page (page2.html)
+app.get("/page2", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "page2.html"));
 });
 
-// Serve the password challenge
-app.get('/page3', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'page3.html'));
+// Route to serve the password challenge (page3.html)
+app.get("/page3", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "page3.html"));
 });
 
-// Handle password submission
-app.post('/check-password', express.urlencoded({ extended: true }), (req, res) => {
+// Handle the password check
+app.post("/check-password", (req, res) => {
   const password = req.body.password;
 
-  if (password === 'unlock') {
-    res.redirect('/success');
+  if (password === "unlock") {
+    res.redirect("/success");
   } else {
-    res.send('Incorrect password. Try again!');
+    res.send("Incorrect password. Try again!");
   }
 });
 
 // Success page (final page with downloadable file)
-app.get('/success', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'success.html'));
+app.get("/success", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "success.html"));
 });
 
 // Provide the downloadable treasure file
-app.get('/download/treasure.txt', (req, res) => {
-  res.download(path.join(__dirname, 'public', 'treasure.txt'));
+app.get("/download/treasure.txt", (req, res) => {
+  res.download(path.join(__dirname, "public", "treasure.txt"));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+// Vercel requires an export of the server as a function
+module.exports = app;
