6db37e754bc5804adf6d0340e711b5c32237be73
Ayush Pandey <137801380+AyushPandey003@users.noreply.github.com>
2024-12-10 11:06:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,1 @@
-module.exports = (req, res) => {
-  const { answer } = req.body;
 
-  // Correct answer for the riddle
-  const correctAnswer = 'egg';
-
-  if (answer.toLowerCase() === correctAnswer) {
-      res.redirect(302, '/success'); // Redirect to success page if the answer is correct
-  } else {
-      res.send('Incorrect answer. Try again!');
-  }
-};
